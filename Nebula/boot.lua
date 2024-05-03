4bebc1c74e9c8850231437a9f628de7d4a8ee6a7
Mag1cpunch <nexonalive@gmail.com>
2024-05-03 16:39:45 +0300

    Update boot.lua

--- Nebula/boot.lua
+++ Nebula/boot.lua
@@ -1,6 +1,6 @@
 local function Main()
     term.clear()
-    paintutils.drawFilledBox(50, 50, 100, 100, 0xffffff)
+    paintutils.drawFilledBox(50, 50, 100, 100, colors.white)
     while true do
         os.sleep(1)
     end
